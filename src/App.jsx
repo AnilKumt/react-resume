6086ee88113e5116a9eaae5718a0a910b357da46
AnilKumt <anilkumt51@gmail.com>
2025-02-11 20:49:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+/**
+ * Application component
+ *
+ * To contain application wide settings, routes, state, etc.
+ */
+
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Anil Kumawat",
+  title: "Web developer",
+  email: "anilkumt51@gmail.com",
+  gitHub: "anilkumt",
+  instagram: "microsoft",
+  linkedIn: "",
+  medium: "",
+  twitter: "microsoft",
+  youTube: "Code",
+};
+
+const primaryColor = "Black";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
+  );
+};
+
+export default App;
