1c3cdde7bf44275f08b3f1aa54caf0ac9130756f
AnilKumt <anilkumt51@gmail.com>
2025-02-12 18:52:58 +0530

    solved errors

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-resume/',
+  base: './',
   build: {
-    outDir: 'root', // This is important for GitHub Pages
+    outDir: 'dist', // This is important for GitHub Pages
   },
 })
